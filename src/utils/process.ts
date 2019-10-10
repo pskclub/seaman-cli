b800a1376c0f4b1c94aa1c03b660529d421e002c
Passakon Puttasuwan <passakon_p@hotmail.com>
2019-10-10 15:53:31 +0700

    Remove log

--- src/utils/process.ts
+++ src/utils/process.ts
@@ -4,7 +4,6 @@ export class Process {
   static run = (cmd: string): Promise<any> => {
     return new Promise((resolve, reject) => {
       exec(cmd, (_, stdout, stderr) => {
-        console.log(stdout)
         resolve(stdout)
       })
     })
